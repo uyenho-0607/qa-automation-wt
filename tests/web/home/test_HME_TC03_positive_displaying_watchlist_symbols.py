ccd1bcb2b127edc6f0ae7aedaaf9bb8feb790507
Ho Ngoc Uyen <ngocuyen.ho@aquariux.com>
2025-07-21 13:55:52 +0700

    update failed script + remove redundant cases

--- tests/web/home/test_HME_TC03_positive_displaying_watchlist_symbols.py
+++ tests/web/home/test_HME_TC03_positive_displaying_watchlist_symbols.py
@@ -9,7 +9,7 @@ from src.utils.logging_utils import logger
 
 
 @pytest.mark.critical
-@pytest.mark.parametrize("tab", WatchListTab.list_values())
+@pytest.mark.parametrize("tab", WatchListTab.list_values(except_val=WatchListTab.ALL))
 def test(web, tab, setup_test):
     exp_symbols = setup_test(tab)
 
