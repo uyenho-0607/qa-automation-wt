58b3dd9cf6f5ac2ed8223e2b3f571b7a66642e59
Ho Ngoc Uyen <ngocuyen.ho@aquariux.com>
2025-07-28 13:05:21 +0700

    remove redundant fixture

--- tests/web/conftest.py
+++ tests/web/conftest.py
@@ -28,27 +28,6 @@ def login_member_site(web):
 
 
 @pytest.fixture(scope="package")
-def web():
-    logger.info("- Init Web Driver")
-    DriverManager.get_driver()
-
-    yield WebContainer()
-
-    logger.info("- Clean up Web Driver")
-    DriverManager.quit_driver()
-
-
-@pytest.fixture(scope="package")
-def login_member_site(web):
-    logger.info("- Navigate to WT Member Site")
-    web.login_page.goto()
-
-    logger.info("- Login to Member Site")
-    web.login_page.login(wait=True)
-    web.home_page.feature_announcement_modal.got_it()
-
-
-@pytest.fixture(scope="package")
 def disable_OCT():
     logger.info("- Check and disable OCT")
     APIClient().user.patch_oct(enable=False)
