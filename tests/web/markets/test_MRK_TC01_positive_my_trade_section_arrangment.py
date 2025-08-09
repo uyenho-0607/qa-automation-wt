d58d9b0443aa973a3bc09ff0c27fe5de70301d43
Ho Ngoc Uyen <ngocuyen.ho@aquariux.com>
2025-08-09 09:14:59 +0700

    stabilize tests

--- tests/web/markets/test_MRK_TC01_positive_my_trade_section_arrangment.py
+++ tests/web/markets/test_MRK_TC01_positive_my_trade_section_arrangment.py
@@ -7,34 +7,34 @@ from src.utils.logging_utils import logger
 
 
 @pytest.mark.critical
-def test(web, disable_OCT):
-    logger.info("Step 1: Get current open positions on Assets Page")
-    cur_symbols = web.assets_page.asset_tab.get_symbols()[:5]
+def test(web, disable_OCT, setup_test):
 
-    logger.info(f"Step 2: Navigate to Market Page and check My Trade Section")
+    cur_symbols = setup_test
+
+    logger.info(f"Step 1: Navigate to Market Page and check My Trade Section")
     web.home_page.navigate_to(Features.MARKETS, wait=True)
 
     logger.info(f"Verify symbols list displayed in My Trade are: {cur_symbols}")
     web.markets_page.verify_my_trade_items_list(cur_symbols)
 
-    logger.info(f"Step 3: Place Pending Order for symbol: {cur_symbols[-1]}")
+    logger.info(f"Step 2: Place Pending Order for symbol: {cur_symbols[-1]}")
     web.markets_page.select_symbol(MarketsSection.MY_TRADE)
     web.trade_page.place_order_panel.place_order(ObjTrade(symbol=cur_symbols[-1], order_type=OrderType.LIMIT), submit=True)
 
-    logger.info(f"Step 4: Navigate to Market Page")
+    logger.info(f"Step 3: Navigate to Market Page")
     web.home_page.navigate_to(Features.MARKETS, wait=True)
 
     logger.info("Verify displaying symbols are not changed")
     web.markets_page.verify_my_trade_items_list(cur_symbols)
 
-    logger.info("Step 5: Navigate to Asset Page and close order")
+    logger.info("Step 4: Navigate to Asset Page and close order")
     web.markets_page.navigate_to(Features.ASSETS, wait=True)
     web.assets_page.asset_tab.full_close_position(wait=True)
 
-    logger.info("Step 6: Get current symbols after closing order")
+    logger.info("Step 5: Get current symbols after closing order")
     cur_symbols = web.assets_page.asset_tab.get_symbols()[:5]
 
-    logger.info("Step 7: Navigate to Market Page")
+    logger.info("Step 6: Navigate to Market Page")
     web.assets_page.navigate_to(Features.MARKETS, wait=True)
 
     logger.info(f"Verify symbols list displayed in My Trade are: {cur_symbols}")
@@ -49,7 +49,10 @@ def setup_test(web, get_asset_tab_amount, ):
     logger.info("- Navigate to Assets Page")
     web.markets_page.navigate_to(Features.ASSETS, wait=True)
 
-    if not tab_amount:
+    logger.info("- Get current open positions on Assets Page")
+    cur_symbols = web.assets_page.asset_tab.get_symbols()[:5]
+
+    if not cur_symbols:
         logger.info(f"- Create some Markets order")
         for _ in range(5):
             trade_obj = ObjTrade(order_type=OrderType.MARKET)
@@ -57,7 +60,8 @@ def setup_test(web, get_asset_tab_amount, ):
 
         # wait for loading new created data
         web.trade_page.asset_tab.wait_for_tab_amount(AssetTabs.OPEN_POSITION, expected_amount=tab_amount + 5)
+        cur_symbols = web.assets_page.asset_tab.get_symbols()[:5]
 
-    yield
+    yield cur_symbols
     logger.info("- Navigate back to markets page")
     web.home_page.navigate_to(Features.MARKETS)
