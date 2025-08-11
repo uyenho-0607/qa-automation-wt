38e3c39997e8084164ab0729f84f39447443ecbb
Ho Ngoc Uyen <ngocuyen.ho@aquariux.com>
2025-08-11 12:00:11 +0700

    update failed script - web-app

--- tests/web_app/home/test_HME_TC01_positive_account_health.py
+++ tests/web_app/home/test_HME_TC01_positive_account_health.py
@@ -10,12 +10,13 @@ from src.utils.logging_utils import logger
 
 @pytest.mark.critical
 def test(web_app, setup_test):
-    exp_account_summary, acc_details = setup_test
+    # acc_details = setup_test
 
     logger.info("Step 1: Open My Account modal")
     web_app.home_page.open_my_account()
 
     logger.info("Verify account summary against api data")
+    exp_account_summary = APIClient().statistics.get_account_statistics(get_acc_balance=True)
     web_app.home_page.my_account_modal.verify_account_info(exp_account_summary)
 
     logger.info(f"Verify Available Account = {exp_account_summary.get(AccSummary.BALANCE)}")
@@ -28,11 +29,10 @@ def setup_test(web_app, symbol):
     trade_object = ObjTrade(order_type=OrderType.MARKET, symbol=symbol)
     APIClient().trade.post_order(trade_object)
 
-    logger.info("- Get account details API details")
-    account_summary = APIClient().statistics.get_account_statistics(get_acc_balance=True)
-    account_details = APIClient().user.get_user_account()
+    # logger.info("- Get account details API details")
+    # account_details = APIClient().user.get_user_account()
 
-    yield account_summary, account_details
+    yield
 
     logger.info("- Close My Account modal")
     web_app.home_page.my_account_modal.close()
