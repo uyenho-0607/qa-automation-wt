ab8a59c26232a5a61fb959ef414a832a2a95306d
Ho Ngoc Uyen <ngocuyen.ho@aquariux.com>
2025-08-19 23:59:04 +0700

    remove auto getting symbol

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,7 +8,7 @@ from src.data.objects.symbol_obj import ObjSymbol
 from src.utils.logging_utils import logger
 
 
-@pytest.fixture(scope="package", autouse=True)
+@pytest.fixture(scope="package")
 def symbol():
     symbols = ObjSymbol().get_symbols()
     logger.debug(f"- Symbols getting result: {symbols}")
