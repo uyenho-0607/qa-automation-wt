d41f4395ff113783d5e06ffdda3cad7129a8ceff
Ho Ngoc Uyen <ngocuyen.ho@aquariux.com>
2025-09-03 09:06:18 +0700

    update failed script android

--- tests/android/conftest.py
+++ tests/android/conftest.py
@@ -2,6 +2,7 @@ import time
 
 import pytest
 
+from src.apis.api_client import APIClient
 from src.core.actions.mobile_actions import MobileActions
 from src.core.driver.appium_driver import AppiumDriver
 from src.core.driver.driver_manager import DriverManager
@@ -13,9 +14,11 @@ from src.utils.logging_utils import logger
 def android():
     logger.info("- Init Android driver")
     DriverManager.get_driver()
-    actions = MobileActions()
 
-    yield AndroidContainer(actions)
+    logger.info("- Init API client")
+    APIClient()
+
+    yield AndroidContainer()
 
     logger.info("- Clean up Android driver")
     DriverManager.quit_driver()
