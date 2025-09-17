2270cf09a66c0f184a80e842f58faeeaac58a430
Ho Ngoc Uyen <ngocuyen.ho@aquariux.com>
2025-09-17 23:31:41 +0700

    refactor android

--- tests/android/home/test_HME_TC03_positive_displaying_watchlist_symbols.py
+++ tests/android/home/test_HME_TC03_positive_displaying_watchlist_symbols.py
@@ -1,3 +1,5 @@
+import random
+
 import pytest
 
 from src.apis.api_client import APIClient
@@ -11,31 +13,50 @@ from src.utils.logging_utils import logger
 def test(android, tab, setup_test):
     exp_symbols = setup_test(tab)
 
-    logger.info(f"Step 1: Select tab {tab.value}")
-    android.trade_screen.watch_list.select_tab(tab)
+    if not exp_symbols:
+        pytest.skip("No symbols to test")
 
-    logger.info(f"Verify current tab displays {len(exp_symbols)} symbols")
-    android.trade_screen.watch_list.verify_symbols_list(symbols=exp_symbols)
+    logger.info("Step 1: Navigate to Home Screen")
+    android.trade_screen.navigate_to(Features.HOME)
 
-    logger.info("Step 2: Get random symbol to select")
-    select_symbol = android.trade_screen.watch_list.get_current_symbols(random_symbol=True)
+    logger.info(f"Step 2: Select tab: {tab.value.title()}")
+    android.home_screen.watch_list.select_tab(tab)
 
-    logger.info(f"Step 3: Select {select_symbol!r}")
-    android.trade_screen.watch_list.select_symbol(select_symbol)
+    logger.info(f"Verify current tab displays {len(exp_symbols)} symbols: {', '.join(exp_symbols)}")
+    android.home_screen.watch_list.verify_symbols_list(exp_symbols)
+
+    logger.info("Step 3: Get random displaying symbol")
+    select_symbol = android.home_screen.watch_list.get_current_symbols(random_symbol=True)
+
+    logger.info(f"Step 4: Select {select_symbol!r}")
+    android.home_screen.watch_list.select_symbol(select_symbol)
 
     logger.info("Verify symbol is selected")
-    android.trade_screen.watch_list.verify_symbol_selected(select_symbol)
+    android.trade_screen.chart.verify_symbol_selected(select_symbol)
 
 
 @pytest.fixture
-def setup_test():
+def setup_test(android):
     def _handler(tab):
 
-        logger.info(f"- Get symbols should be displayed in tab: {tab}")
-        symbols = APIClient().market.get_watchlist_items(tab, get_symbols=True)
+        logger.info(f"{'=' * 10} Setup Test - Start {'=' * 10}")
+        if tab != WatchListTab.FAVOURITES:
+            logger.info(f"- Get symbols should be displayed in tab: {tab}")
+            symbols = APIClient().market.get_watchlist_items(tab, get_symbols=True)
 
-        if not symbols and tab == WatchListTab.FAVOURITES:
-            _list_symbol = ObjSymbol().all_symbols[:3]
+            # Filter and sample symbols to avoid excessive scrolling
+            if symbols and len(symbols) > 10:
+                # filter out symbols placed at the end of the list
+                filtered_symbol = [sym for sym in symbols if not any(sym.startswith(letter) for letter in ['U', 'V', 'W', 'X', 'Y', 'Z'])]
+                if filtered_symbol:
+                    symbols = filtered_symbol
+
+            symbols = random.sample(symbols, 10) if len(symbols) >= 10 else symbols
+
+        else:
+
+            logger.info(f"- Prepare starred symbols for checking tab {WatchListTab.FAVOURITES.value!r}")
+            _list_symbol = ObjSymbol(amount=10).all_symbols
 
             for symbol in _list_symbol:
                 logger.info(f"- Mark star symbol: {symbol!r}")
@@ -43,18 +64,17 @@ def setup_test():
 
             symbols = _list_symbol
 
+        logger.info(f">> Setup Summary: List symbols should be displayed in tab: {tab.value!r} - {', '.join(symbols)!r}")
+        logger.info(f"{'=' * 10} Setup Test - Done {'=' * 10}")
+
         return symbols
 
     return _handler
 
 
 @pytest.fixture(autouse=True)
-def cleanup(tab, android):
+def cleanup(tab):
     yield
-
-    logger.info("- Navigate back to Home Screen")
-    android.trade_screen.navigate_to(Features.HOME)
-
     if tab == WatchListTab.FAVOURITES:
-        logger.info(f"- Delete all star symbols")
+        logger.info(f"[Cleanup] Delete all star symbols")
         APIClient().market.delete_starred_symbols()
