9a56aed3addfb359611b67d0a4cc7fe6761b8bd0
Ho Ngoc Uyen <ngocuyen.ho@aquariux.com>
2025-09-17 23:31:41 +0700

    refactor android

--- tests/android/trade/market/test_TRD_MRK_TC09_positive_place_order_compare_against_api_data.py
+++ tests/android/trade/market/test_TRD_MRK_TC09_positive_place_order_compare_against_api_data.py
@@ -1,26 +1,25 @@
 import pytest
 
 from src.apis.api_client import APIClient
-from src.data.enums import AssetTabs, OrderType
-from src.data.objects.trade_obj import ObjTrade
+from src.data.enums import AssetTabs
 from src.utils.logging_utils import logger
 
 
 @pytest.mark.critical
-def test(android, symbol, get_asset_tab_amount, ):
-    trade_object = ObjTrade(order_type=OrderType.MARKET, symbol=symbol)
+def test(android, market_obj, get_asset_tab_amount, cancel_all):
+    trade_object = market_obj()
 
     logger.info("Step 1: Get asset tab amount")
     tab_amount = get_asset_tab_amount(trade_object.order_type)
 
     logger.info(f"Step 2: Place {trade_object.trade_type} order (tab amount: {tab_amount!r})")
-    android.trade_screen.place_order_panel.place_order(trade_object, submit=True)
+    android.trade_screen.place_order_panel.place_order(trade_object, confirm=True)
 
     logger.info(f"Verify Asset Tab amount is: {tab_amount + 1}")
     android.trade_screen.asset_tab.verify_tab_amount(AssetTabs.OPEN_POSITION, tab_amount + 1)
 
     logger.info("Step 3: Get order_id of placed order")
-    android.trade_screen.asset_tab.get_last_order_id(trade_object)
+    trade_object.order_id = android.trade_screen.asset_tab.get_last_order_id(AssetTabs.OPEN_POSITION)
 
     logger.info(f"Step 4: Get placed order API data, order_id: {trade_object.order_id!r}")
     api_data = APIClient().order.get_orders_details(
@@ -30,9 +29,3 @@ def test(android, symbol, get_asset_tab_amount, ):
     logger.info("Verify placed order against API data")
     android.trade_screen.verify_placed_order_data(trade_object, api_data)
 
-
-@pytest.fixture(autouse=True)
-def teardown_test(android):
-    yield
-    logger.info("- Teardown test")
-    android.trade_screen.place_order_panel.click_cancel_btn()
