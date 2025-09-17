850d3bcaba62e36ae010e3d1da45ad28a98cda3f
Ho Ngoc Uyen <ngocuyen.ho@aquariux.com>
2025-09-17 23:31:41 +0700

    refactor android

--- tests/android/trade/market/test_TRD_MRK_TC06_positive_partial_close_position.py
+++ tests/android/trade/market/test_TRD_MRK_TC06_positive_partial_close_position.py
@@ -1,64 +1,55 @@
 import pytest
 
-from src.data.enums import AssetTabs, OrderType, Features
+from src.data.enums import AssetTabs, Features
 from src.data.objects.notification_obj import ObjNoti
-from src.data.objects.trade_obj import ObjTrade
+from src.utils.format_utils import format_display_dict
 from src.utils.logging_utils import logger
 
-
 @pytest.mark.critical
-def test(android, symbol, get_asset_tab_amount, create_order_data):
-    trade_object = ObjTrade(order_type=OrderType.MARKET, symbol=symbol)
+def test(android, market_obj, get_asset_tab_amount, order_data):
+    trade_object = market_obj()
 
-    logger.info("Step 1: Get asset tab amount")
+    logger.info(f"Step 1: Place order with: {format_display_dict(trade_object)}")
+    order_data(trade_object)
+    android.trade_screen.asset_tab.get_last_order_id(trade_object, wait=True)
+
+    logger.info(f"Verify order placed successfully, order_id: {trade_object.order_id!r}")
+    android.trade_screen.asset_tab.verify_item_data(trade_object, AssetTabs.OPEN_POSITION, wait=False)
+
+    logger.info("Step 2: Get asset tab amount")
     tab_amount = get_asset_tab_amount(trade_object.order_type)
 
-    logger.info(f"Step 2: Place {trade_object.trade_type} Order")
-    create_order_data(trade_object)
-
-    logger.info(f"Step 3: Select tab {AssetTabs.POSITIONS_HISTORY.value!r}")
-    android.trade_screen.asset_tab.select_tab(AssetTabs.POSITIONS_HISTORY)
-
-    logger.info(f"Verify order placed successfully, order_id = {trade_object.order_id!r}")
-    android.trade_screen.asset_tab.verify_item_displayed(AssetTabs.OPEN_POSITION, trade_object.order_id)
-
-    # Object for new created open position
-    new_object = ObjTrade(**{k: v for k, v in trade_object.items() if k != "order_id"})
-
-    logger.info("Step 4: Select Open Position tab")
-    android.trade_screen.asset_tab.select_tab(AssetTabs.OPEN_POSITION)
-
-    logger.info("Step 5: Partial close position")
-    android.trade_screen.asset_tab.partial_close_position(new_object, trade_object.order_id, confirm=True)
-
-    # update new volume, units after partial close
-    trade_object.volume, trade_object.units = new_object.close_volume, new_object.close_units
+    logger.info("Step 3: Partial close position")
+    new_object = android.trade_screen.asset_tab.partial_close_position(trade_object)
 
     logger.info(f"Verify close order notification banner")
     exp_noti = ObjNoti(trade_object)
     android.home_screen.notifications.verify_notification_banner(*exp_noti.close_order_success_banner())
 
-    logger.info(f"Verify asset tab amount = {tab_amount + 1}")
-    android.trade_screen.asset_tab.verify_tab_amount(AssetTabs.OPEN_POSITION, tab_amount + 1)
+    logger.info(f"Verify asset tab amount = {tab_amount}")
+    android.trade_screen.asset_tab.verify_tab_amount(AssetTabs.OPEN_POSITION, tab_amount)
 
     logger.info(f"Verify Open Position details in asset tab")
     android.trade_screen.asset_tab.verify_item_data(new_object)
 
-    logger.info(f"Step 6: Select tab {AssetTabs.POSITIONS_HISTORY.value.capitalize()}")
-    android.trade_screen.asset_tab.select_tab(AssetTabs.POSITIONS_HISTORY)
+    logger.info(f"Step 4: Select tab {AssetTabs.HISTORY.value.capitalize()}")
+    android.trade_screen.asset_tab.select_tab(AssetTabs.HISTORY)
 
     logger.info(f"Verify history order item details")
     android.trade_screen.asset_tab.verify_item_data(trade_object, AssetTabs.POSITIONS_HISTORY)
 
-    logger.info("Step 7: Navigate to Home screen")
+    logger.info("Step 5: Navigate to Home screen")
     android.trade_screen.navigate_to(Features.HOME)
 
     logger.info(f"Verify position closed noti in notification box")
-    android.home_screen.notifications.verify_notification_result(exp_noti.position_closed_details())
+    android.home_screen.notifications.verify_notification_result(exp_noti.position_closed_details(), close=True)
 
 
 @pytest.fixture(autouse=True)
-def teardown(android, cancel_close_order):
+def teardown(android, cancel_all):
     yield
-    logger.info("- Navigate to Trade screen")
+    logger.info("- Navigate to Trade screen", teardown=True)
     android.home_screen.navigate_to(Features.TRADE)
+
+    logger.info("- Select Open Positions tab", teardown=True)
+    android.trade_screen.asset_tab.select_tab(AssetTabs.OPEN_POSITION)
