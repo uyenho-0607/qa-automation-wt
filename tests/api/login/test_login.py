491fc43fa80fe909603b917295e45019f93f8d66
Ho Ngoc Uyen <ngocuyen.ho@aquariux.com>
2025-10-01 10:51:09 +0700

    update apis test

--- tests/api/login/test_login.py
+++ tests/api/login/test_login.py
@@ -1,7 +1,16 @@
+import pytest
+
 from src.apis.api_client import APIClient
+from src.data.project_info import RuntimeConfig
 from src.utils.logging_utils import logger
 
-
-def test():
-    logger.debug("Step: Send API login to client")
+@pytest.mark.parametrize("times", range(20))
+def test(times):
+    logger.info("Step: Send API login to client")
     APIClient()
+
+
+@pytest.fixture(autouse=True)
+def cleanup():
+    yield
+    RuntimeConfig.headers = {}
