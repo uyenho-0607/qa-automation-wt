15ce107c186e2c2ca9f2eeacc0e910c74c7d7408
camkeam <camcam.test01@gmail.com>
2025-10-06 14:23:56 +0800

    update MT4 path

--- src/data/consts.py
+++ src/data/consts.py
@@ -15,7 +15,7 @@ CSV_DIR = {
     #Server.MT5: r"~/Library/Application Support/net.metaquotes.wine.metatrader5/drive_c/Program Files/MetaTrader 5/MQL5/Files/",
     Server.MT5: r"C:\Users\thicam.truong\AppData\Roaming\MetaQuotes\Terminal\D0E8209F77C8CF37AD8BF550E51FF075\MQL5\Files",
     #Server.MT5: r"%USERPROFILE%\AppData\Roaming\MetaQuotes\Terminal\D0E8209F77C8CF37AD8BF550E51FF075\MQL5\Files",
-    Server.MT4: r"~/Library/Application Support/net.metaquotes.wine.metatrader4/drive_c/Program Files (x86)/MetaTrader 4/MQL4/Files/"
+    Server.MT4: r"C:\Users\thicam.truong\AppData\Roaming\MetaQuotes\Terminal\51615F5B21EB6EEC08D70B45B8B59F73\MQL4\Files"
 }
 
 # # WINDOWS
